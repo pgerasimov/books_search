2df08512c9614ab8a20ccaf6fb205e303dc43dfa
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-09-24 11:54:57 +0200

    removed unique and added def repr

--- webapp/model.py
+++ webapp/model.py
@@ -8,8 +8,8 @@ db = SQLAlchemy()
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(120))
     last_login = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def set_password(self, password):
@@ -18,36 +18,38 @@ class Users(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
 
 class SearchRequest(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    request_text = db.Column(db.String(120), unique=True, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.datetime.now, nullable=True)
+    request_text = db.Column(db.String(120))
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
     research_id = db.Column(db.Integer)
 
 
 class Authors(db.Model, UserMixin):
     author_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(120), unique=True, nullable=False)
-    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
 
 
 class Books(db.Model, UserMixin):
     book_id = db.Column(db.Integer, primary_key=True)
-    book_name = db.Column(db.String(120), unique=True, nullable=False)
+    book_name = db.Column(db.String(120))
     publication_date = db.Column(db.DateTime, default=datetime.datetime.now)
-    isbn = db.Column(db.String(120), unique=True, nullable=False)
+    isbn = db.Column(db.String(120), unique=True)
     author_id = db.Column(db.Integer)
 
 
 class AdditionalInfo(db.Model, UserMixin):
-    book_image = db.Column(db.String(120), unique=True, nullable=False)
-    book_annotation = db.Column(db.String(120), unique=True, nullable=False)
-    book_genre = db.Column(db.String(120), unique=True, nullable=False)
-    book_publisher = db.Column(db.String(120), unique=True, nullable=False)
-    book_coauthor = db.Column(db.String(120), unique=True, nullable=False)
-    book_bio = db.Column(db.String(120), unique=True, nullable=False)
-    book_img = db.Column(db.String(120), unique=True, nullable=False)
+    book_image = db.Column(db.String(120))
+    book_annotation = db.Column(db.String(120))
+    book_genre = db.Column(db.String(120))
+    book_publisher = db.Column(db.String(120))
+    book_coauthor = db.Column(db.String(120))
+    book_bio = db.Column(db.String(120))
     book_quantity = db.Column(db.Integer)
     author_id = db.Column(db.Integer)
     book_id = db.Column(db.Integer, primary_key=True)
