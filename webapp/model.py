6b55dfc2491d3d12b5868b55f02ddbc0c8c9729f
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-09-24 07:27:50 +0200

    add checking password

--- webapp/model.py
+++ webapp/model.py
@@ -1,30 +1,38 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
-class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=True, nullable=False)
     last_login = db.Column(db.DateTime, default=datetime.datetime.now)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-class SearchRequest(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class SearchRequest(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
     request_text = db.Column(db.String(120), unique=True, nullable=False)
     date = db.Column(db.DateTime, default=datetime.datetime.now, nullable=True)
     research_id = db.Column(db.Integer)
 
 
-class Authors(db.Model):
+class Authors(db.Model, UserMixin):
     author_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120), unique=True, nullable=False)
     last_name = db.Column(db.String(120), unique=True, nullable=False)
 
 
-class Books(db.Model):
+class Books(db.Model, UserMixin):
     book_id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(120), unique=True, nullable=False)
     publication_date = db.Column(db.DateTime, default=datetime.datetime.now)
@@ -32,7 +40,7 @@ class Books(db.Model):
     author_id = db.Column(db.Integer)
 
 
-class AdditionalInfo(db.Model):
+class AdditionalInfo(db.Model, UserMixin):
     book_image = db.Column(db.String(120), unique=True, nullable=False)
     book_annotation = db.Column(db.String(120), unique=True, nullable=False)
     book_genre = db.Column(db.String(120), unique=True, nullable=False)
