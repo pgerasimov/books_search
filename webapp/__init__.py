dc5792c5380452c3a21189c87d5e542f227a217d
Pavel Gerasimov <gerasimov86@gmail.com>
2019-09-24 23:21:06 +0300

    add index route / add is_authenticated / add registration func / refactoring

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -19,43 +19,74 @@ def create_app():
     def load_user(user_id):
         return Users.query.get(user_id)
 
+    @app.route("/")
+    def index():
+        return render_template('base.html')
+
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            flash('Вы уже авторизованы')
+            return redirect(url_for('index'))
         title = "Авторизация"
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
 
-    @app.route('/registration')
-    def registration():
-        title = "Регистрация"
-        login_form = RegistrationForm()
-        return render_template('registration.html', page_title=title, form=login_form)
-
-    @app.route('/process-registration', methods=['POST'])
-    def process_registration():
-        form = RegistrationForm()
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
         if form.validate_on_submit():
-            user = Users.query.filter_by(username=form.email.data).first()
-            if user and Users.check_password(form.password.data):
+            user = Users.query.filter_by(email=form.username.data).first()
+            if user and user.check_password(form.password.data):
                 login_user(user)
                 flash('Вы вошли на сайт')
-                return redirect(url_for('registration'))
+                return redirect(url_for('index'))
 
         flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/registration')
+    def registration():
+        if current_user.is_authenticated:
+            flash('Вы уже авторизованы')
+            return redirect(url_for('index'))
+        title = "Регистрация"
+        registration_form = RegistrationForm()
+        return render_template('registration.html', page_title=title, form=registration_form)
+
+    @app.route('/process_registration', methods=['POST'])
+    def process_registration():
+        form = RegistrationForm()
+        if form.submit_reg():
+            username = form.username_reg.data
+            print(username)
+            if Users.query.filter(Users.email == username).count():
+                flash('Такой пользователь уже есть')
+                return redirect(url_for('registration'))
+            password = form.password_reg.data
+            new_user = Users(email=username)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+            print('User with id {} added'.format(new_user.email))
+            flash('Вы успешно зарегистрировались')
+            return redirect(url_for('index'))
+
+        flash('Форма не провалидировалась')
         return redirect(url_for('registration'))
 
     @app.route('/logout')
     def logout():
         logout_user()
         flash('Вы успешно разлогинились')
-        return redirect(url_for('registration'))
+        return redirect(url_for('index'))
 
-    @app.route('/admin')
-    @login_required
-    def admin_index():
-        if current_user.is_admin:
-            return 'Привет админ'
-        else:
-            return 'Ты не админ!'
+    # @app.route('/admin')
+    # @login_required
+    # def admin_index():
+    #     if current_user.is_admin:
+    #         return 'Привет админ'
+    #     else:
+    #         return 'Ты не админ!'
 
     return app
