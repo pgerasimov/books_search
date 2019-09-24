22e9891f3a54e23808826c002cc23396a07d07e7
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-09-24 14:40:17 +0200

    added process-registration

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user
 from webapp.forms import LoginForm, RegistrationForm
 from flask_migrate import Migrate
-from webapp.model import db
+from webapp.model import db, Users, SearchRequest, Authors, Books, AdditionalInfo
 
 
 def create_app():
@@ -9,6 +10,14 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'registration'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(user_id)
 
     @app.route('/login')
     def login():
@@ -21,5 +30,18 @@ def create_app():
         title = "Регистрация"
         login_form = RegistrationForm()
         return render_template('registration.html', page_title=title, form=login_form)
+
+    @app.route('/process-registration', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = Users.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
 
     return app
