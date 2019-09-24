3d707b7806402c8f26393804d15e95f75fda2022
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-09-24 07:29:57 +0200

    add RegistrationForm

--- webapp/forms.py
+++ webapp/forms.py
@@ -8,4 +8,12 @@ class LoginForm(FlaskForm):
                            render_kw={"class": "form-control", "placeholder": "Enter email", "type": "email"})
     password = PasswordField('Пароль', validators=[DataRequired()],
                              render_kw={"class": "form-control", "placeholder": "Enter Password", "type": "password"})
-    submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary", "Type":"submit"})
+    submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary", "Type": "submit"})
+
+
+class RegistrationForm(FlaskForm):
+    username_reg = StringField('Имя пользователя', validators=[DataRequired()],
+                           render_kw={"class": "form-control", "placeholder": "Enter email", "type": "email"})
+    password_reg = PasswordField('Пароль', validators=[DataRequired()],
+                             render_kw={"class": "form-control", "placeholder": "Enter Password", "type": "password"})
+    submit_reg = SubmitField('Отправить', render_kw={"class": "btn btn-primary", "Type": "submit"})
