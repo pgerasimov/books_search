f4bdf4cbdb5f69083982a4f6f7a5c2ef0641b821
Pavel <gerasimov86@gmail.com>
2019-09-25 11:50:08 +0300

    change button names and labels

--- webapp/forms.py
+++ webapp/forms.py
@@ -4,17 +4,17 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()],
+    username = StringField('Email', validators=[DataRequired()],
                            render_kw={"class": "form-control", "placeholder": "Enter email", "type": "email"})
-    password = PasswordField('Пароль', validators=[DataRequired()],
+    password = PasswordField('Password', validators=[DataRequired()],
                              render_kw={"class": "form-control", "placeholder": "Enter Password", "type": "password"})
-    submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary", "Type": "submit"})
+    submit = SubmitField('Войти', render_kw={"class": "btn btn-primary", "Type": "submit"})
 
 
 class RegistrationForm(FlaskForm):
-    username_reg = StringField('Имя пользователя', validators=[DataRequired()],
+    username_reg = StringField('Email', validators=[DataRequired()],
                                render_kw={"class": "form-control", "placeholder": "Enter email", "type": "email"})
-    password_reg = PasswordField('Пароль', validators=[DataRequired()],
+    password_reg = PasswordField('Password', validators=[DataRequired()],
                                  render_kw={"class": "form-control", "placeholder": "Enter Password",
                                             "type": "password"})
-    submit_reg = SubmitField('Отправить', render_kw={"class": "btn btn-primary", "Type": "submit"})
+    submit_reg = SubmitField('Зарегистрироваться', render_kw={"class": "btn btn-primary", "Type": "submit"})
