a3befb4cb8de882bbf0faadccb7a6095bab43d42
Pavel <gerasimov86@gmail.com>
2019-09-25 16:04:53 +0300

    code style

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -37,12 +37,14 @@ def create_app():
         form = LoginForm()
         if form.validate_on_submit():
             user = Users.query.filter_by(email=form.username.data).first()
+
             if user and user.check_password(form.password.data):
                 login_user(user)
                 flash('Вы вошли на сайт')
                 return redirect(url_for('index'))
+
             else:
-                flash('Неправильное имя пользователя или парол')
+                flash('Неправильное имя пользователя или пароль')
                 return redirect(url_for('login'))
 
     @app.route('/registration')
@@ -50,35 +52,39 @@ def create_app():
         if current_user.is_authenticated:
             flash('Вы уже авторизованы')
             return redirect(url_for('index'))
+
         title = "Регистрация"
         registration_form = RegistrationForm()
         return render_template('registration.html', page_title=title, form=registration_form)
 
     @app.route('/process_registration', methods=['POST'])
     def process_registration():
+
         form = RegistrationForm()
-        if form.submit_reg():
+        if form.validate_on_submit():
+
             username = form.username_reg.data
-            print(username)
+            password = form.password_reg.data
+
             if Users.query.filter(Users.email == username).count():
                 flash('Такой пользователь уже есть')
                 return redirect(url_for('registration'))
-            password = form.password_reg.data
+
             new_user = Users(email=username)
             new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
-            print('User with id {} added'.format(new_user.email))
+
             flash('Вы успешно зарегистрировались')
             return redirect(url_for('index'))
 
-        flash('Форма не провалидировалась')
+        flash('Пароль должен содеражть хотя бы одну заглавную букву, хотя бы одну цифру и быть не менее 8 символов')
         return redirect(url_for('registration'))
 
     @app.route('/logout')
     def logout():
         logout_user()
-        flash('Вы успешно разлогинились')
+        flash('Вы успешно вышли из системы')
         return redirect(url_for('index'))
 
     # @app.route('/admin')
