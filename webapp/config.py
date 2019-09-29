c14661f3a966574eb185ef5c8efe885372256619
Pavel Gerasimov <gerasimov86@gmail.com>
2019-09-29 18:29:26 +0300

    get token from api

--- webapp/config.py
+++ webapp/config.py
@@ -1,5 +1,7 @@
 import os
 
+from webapp.api import get_token
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
@@ -7,3 +9,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db'
 SECRET_KEY = os.urandom(24)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+token = get_token()
