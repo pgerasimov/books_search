55473ddae344eb1dd3c9973ec17c0e1a9f915482
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-09-30 13:18:48 +0200

    deleted comments and def process_search

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,8 +4,6 @@ from webapp.forms import LoginForm, RegistrationForm, SearchForm
 from flask_migrate import Migrate
 from webapp.model import db, Users, SearchRequest, Authors, Books
 import logging
-import goodreads_api_client as gr
-
 
 def create_app():
     app = Flask(__name__)
@@ -85,14 +83,6 @@ def create_app():
             new_user = Users(email=username)
             new_user.set_password(password)
             db.session.add(new_user)
-            # new_book = Books(book_name='Chris Nord', isbn='343243243', book_publisher='MoscowLit', book_genre='Роман', book_annotation='Если бы не звон буддийского колокольчика по ночам, если бы не ')
-            # db.session.add(new_book)
-            # new_author = Authors(name='Jack Smith', author_bio='Родился в 1949 году в Киото, окончил школу, женился на однокласснице и открыл бар.')
-            # db.session.add(new_author)
-            # news_book = Books(book_name='Tailor', isbn='343243241', book_publisher='Piter', book_genre='Приключения', book_annotation='Книга о том, как биохакеры, трансгуманисты, ученные и миллиардеры пытаются решить проблему смерти с помощью технологий и искусственного интеллекта.')
-            # db.session.add(news_book)
-            # news_author = Authors(name='Bill Grindge', author_bio='Занимался литературным переводом с японского и английского языков, был заместителем главного редактора журнала')
-            # db.session.add(news_author)
             db.session.commit()
 
             flash('Вы успешно зарегистрировались')
@@ -114,35 +104,5 @@ def create_app():
         title = "Поиск книги"
         search_form = SearchForm()
         return render_template('search.html', page_title=title, form=search_form)
-
-    @app.route('/process-search', methods=['POST'])
-    @login_required
-    def process_search():
-        search_form = SearchForm()
-        if search_form.validate_on_submit():
-            for i in range(len(db.session.query(Books.book_name).all())):
-                if (db.session.query(Books.book_name).all()[i][0] == search_form.search_by_book_name.data) and search_form.search_by_book_name.data != '':
-                    if (db.session.query(Books.book_name).all()[i][0] == search_form.search_by_book_name.data) and search_form.search_by_book_name.data != '':
-                        responses = db.session.query(Books).join(Authors, Authors.id == Books.author_id).all()
-                        for item in responses:
-                            result = {'Название книги': item.book_name,
-                                    'Автор': item.thread.name,
-                                    'Издательство': item.book_publisher,
-                                    'Аннотация': item.book_annotation,
-                                    'Жанр': item.book_genre,
-                                    'Биография автора': item.thread.author_bio}
-                            return render_template('response.html', data=result)
-
-            # for i in range(len(db.session.query(Authors.name).all())):
-            #     if (db.session.query(Authors.name).all()[i][0] == search_form.search_by_author_name.data) and search_form.search_by_author_name.data != '':
-            #         response = db.session.query(Authors).filter_by(name=search_form.search_by_author_name.data)
-            #         for record in response:
-            #             return render_template('response.html', data=record.__dict__)
-
-            # for i in range(len(db.session.query(Books.isbn).all())):
-            #     if db.session.query(Books.isbn).all()[i][0] == search_form.search_by_ISBN.data:
-            #         flash('Книга с таким IBSN есть в нашей базе данных!')
-
-        return redirect(url_for('search'))
 
     return app
