4be0efef0bf7a2005b1ef1be056971c671990ba0
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-22 11:31:02 +0200

    added test_new_books

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-from webapp.model import db, Users
+from webapp.model import db, Users, Books
 import pytest
 from flask import Flask
 from webapp import create_app
@@ -29,8 +29,6 @@ def init_database():
 
         yield db
 
-        # db.drop_all()
-
 def test_new_user(new_user, init_database):
     assert new_user.email == 'serg@gmail.com'
     assert new_user.password == '123456789asdf'
@@ -53,9 +51,7 @@ def test_client():
 
 
 def test_home_page(test_client):
-    print(2323)
     response = test_client.get('/')
-    print(response.status_code)
     assert response.status_code == 200
 
 
@@ -66,21 +62,33 @@ def test_valid_login_logout(test_client):
     response = test_client.get('/logout', follow_redirects=True)
     assert response.status_code == 200
 
+
+def test_valid_registration(test_client):
+    response = test_client.get('/registration', follow_redirects=True)
+    assert response.status_code == 200
+
 #  Test books
-# @pytest.fixture()
-# def init_db_book():
-#     db.create_all()
+@pytest.fixture(scope='module')
+def add_books():
 
-#     book1 = Books(book_name='uniq book 7777', isbn='7777', book_publisher='Test Corp 7777', book_genre='7777',
-#                      book_annotation='Уникальное описание книги 7777', author_id='7777')
-#     book2 = Books(book_name='uniq book 8888', isbn='8888', book_publisher='Test Corp 8888', book_genre='8888',
-#                      book_annotation='Уникальное описание книги 8888', author_id='8888')
+    with app.app_context():
+        db.create_all()
 
-#     db.session.add(book1)
-#     db.session.add(book2)
+        book1 = Books(book_name='uniq book 7777', isbn='7777', book_publisher='Test Corp 7777', book_genre='7777',
+                        book_annotation='Уникальное описание книги 7777', author_id='7777')
+        book2 = Books(book_name='uniq book 8888', isbn='8888', book_publisher='Test Corp 8888', book_genre='8888',
+                        book_annotation='Уникальное описание книги 8888', author_id='8888')
 
-#     db.session.commit()
+        db.session.add(book1)
+        db.session.add(book2)
 
-#     yield db
+        db.session.commit()
 
-    # db.drop_all()
+        yield db
+
+
+def test_new_books(add_books, init_database):
+    book1_db = Books.query.filter_by(book_name='uniq book 7777')[0]
+    book2_db = Books.query.filter_by(book_name='uniq book 8888')[0]
+    assert book1_db.book_name == 'uniq book 7777'
+    assert book2_db.book_name == 'uniq book 8888'
