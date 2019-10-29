6ff22427fe7ea933deb267160763f182bfd3906e
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-29 20:00:15 +0100

    fixed according to flake8

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Regexp, EqualTo
+from wtforms.validators import DataRequired, Regexp
 
 regexp = r'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\S+]{8,}'
 
