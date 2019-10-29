82f0be7397b39547d52551e37c4c45ce47b285df
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-29 20:51:28 +0100

    fixed according to flake8

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,24 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Regexp
+from wtforms.validators import DataRequired, Regexp, EqualTo
 
 regexp = r'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\S+]{8,}'
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Email', validators=[DataRequired()],
-                           render_kw={
-                               "class": "form-control",
-                               "placeholder": "Enter email",
-                               "type": "email"})
-    password = PasswordField('Password', validators=[DataRequired()],
-                             render_kw={
-                                 "class": "form-control",
-                                 "placeholder": "Enter Password",
-                                 "type": "password"})
-    submit = SubmitField('Войти', render_kw={
-                                            "class": "btn btn-primary",
-                                            "Type": "submit"})
+    username = StringField(
+        'Email',
+        validators=[DataRequired()],
+        render_kw={
+                    "class": "form-control",
+                    "placeholder": "Enter email",
+                    "type": "email"})
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()],
+        render_kw={
+                    "class": "form-control",
+                    "placeholder": "Enter Password",
+                    "type": "password"})
+    submit = SubmitField(
+        'Войти',
+        render_kw={
+                    "class": "btn btn-primary",
+                    "Type": "submit"})
     remember_me = BooleanField(
                                 'Запомнить меня',
                                 default=True,
@@ -26,27 +32,30 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    username_reg = StringField('Email', validators=[DataRequired()],
-                               render_kw={
-                                   "class": "form-control",
-                                   "placeholder": "Enter email",
-                                   "type": "email"})
-    password_reg = PasswordField('Password', validators=[
-                                                        DataRequired(),
-                                                        Regexp(regexp)],
-                                 render_kw={
-                                     "class": "form-control",
-                                     "placeholder": "Enter Password",
-                                     "type": "password"})
+    username_reg = StringField(
+        'Email',
+        validators=[DataRequired()],
+        render_kw={
+                    "class": "form-control",
+                    "placeholder": "Enter email",
+                    "type": "email"})
+    password_reg = PasswordField(
+        'Password',
+        validators=[
+                    DataRequired(),
+                    Regexp(regexp)
+                ],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter Password",
+            "type": "password"})
     password_reg_confirm = PasswordField(
-                                'Password',
-                                validators=[
-                                            DataRequired(),
-                                            Regexp(regexp)],
-                                render_kw={
-                                     "class": "form-control",
-                                     "placeholder": "Confirm Password",
-                                     "type": "password"})
+        'Password',
+        validators=[
+                    DataRequired(),
+                    Regexp(regexp),
+                    EqualTo('password_reg')
+                ])
     submit_reg = SubmitField(
                             'Зарегистрироваться',
                             render_kw={
